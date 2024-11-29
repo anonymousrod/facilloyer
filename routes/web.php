e759b54f544e563004d051d96c491e2c43ca3ce8
teteganexauce <teteganexauce@gmail.com>
2024-11-29 01:50:44 +0100

    first dash template integrate

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('layouts.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
