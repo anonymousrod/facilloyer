252b8ea592c2e0f9949261181025f6b6a8e8b633
teteganexauce <teteganexauce@gmail.com>
2024-12-30 04:09:02 +0100

    photo, rccm, carte identité reglé

--- resources/views/layouts/layouts_dash/head.blade.php
+++ resources/views/layouts/layouts_dash/head.blade.php
@@ -16,26 +16,26 @@
     <link href="{{asset('assets/css/app.min.css')}} " rel="stylesheet" type="text/css" />
 
     {{-- head login --}}
-    <meta charset="utf-8" />
+    {{-- <meta charset="utf-8" /> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="" name="author" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    {{-- <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" /> --}}
+    {{-- <meta content="" name="author" /> --}}
+    {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> --}}
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
 
     <!-- App css -->
-    <link href=" {{asset('assets/css/bootstrap.min.css')}} " rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/icons.min.css')}} " rel="stylesheet" type="text/css" />
+    {{-- <link href=" {{asset('assets/css/bootstrap.min.css')}} " rel="stylesheet" type="text/css" /> --}}
+    {{-- <link href="{{asset('assets/css/icons.min.css')}} " rel="stylesheet" type="text/css" /> --}}
 
     {{-- head dashboard --}}
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="" name="author" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    {{-- <meta charset="utf-8" /> --}}
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
+    {{-- <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" /> --}}
+    {{-- <meta content="" name="author" /> --}}
+    {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> --}}
     {{-- gestion de calandar script  --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -55,14 +55,14 @@
     {{-- c'est pour les tables --}}
 
 
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="" name="author" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    {{-- <meta charset="utf-8" /> --}}
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
+    {{-- <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" /> --}}
+    {{-- <meta content="" name="author" /> --}}
+    {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> --}}
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    {{-- <link rel="shortcut icon" href="assets/images/favicon.ico"> --}}
 
 
     <link href="assets/libs/simple-datatables/style.css" rel="stylesheet" type="text/css" />
