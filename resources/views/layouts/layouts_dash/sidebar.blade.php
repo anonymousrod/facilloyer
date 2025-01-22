14860664f8cd7b2230a88dfce1870cadd627b306
unknown <rodriguebet@gmail.com>
2025-01-22 15:12:11 +0100

    derniere souci regler a pusher sur rod mtn

--- resources/views/layouts/layouts_dash/sidebar.blade.php
+++ resources/views/layouts/layouts_dash/sidebar.blade.php
@@ -99,6 +99,12 @@
                                 <span>Voir les statistiques</span>
                             </a>
                         </li><!--end nav-item-->
+                        <li class="nav-item">
+                                <a class="nav-link" href="t.t">
+                                    <i class="iconoir-chat-bubble menu-icon"></i> <!-- Remplacé par une icône de chat -->
+                                    <span>Assistance en ligne</span>
+                                </a>
+                            </li><!--end nav-item-->
                     @endif
 
 
@@ -191,7 +197,7 @@
 
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('user', Auth::user()->locataires->first()->agent_immobilier->user->id) }}">
-                                <i class="iconoir-phone menu-icon"></i> <!-- Icône pour messagerie -->
+                                <i class="iconoir-chat-bubble menu-icon"></i> <!-- Icône pour messagerie -->
                                 <span>Assistance en ligne</span>
                             </a>
                         </li>
@@ -309,11 +315,19 @@
 
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('user', $user->first()->id) }}">
-                                    <i class="iconoir-view-grid menu-icon"></i>
-                                    <span>Messagerie</span>
+                                    <i class="iconoir-chat-bubble menu-icon"></i> <!-- Remplacé par une icône de chat -->
+                                    <span>Assistance en ligne</span>
                                 </a>
-
                             </li><!--end nav-item-->
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="Y.Y">
+                                    <i class="iconoir-bell menu-icon"></i> <!-- Icône pour les notifications -->
+                                    <span>Notifications</span>
+                                </a>
+                            </li><!--end nav-item-->
+
+
 
                         @else
                             <!-- Si l'agent n'est pas validé -->
