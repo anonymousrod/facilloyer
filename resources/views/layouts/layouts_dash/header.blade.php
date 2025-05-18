5ef964274357ad1af93cff7490b6f74fb36db033
teteganexauce <teteganexauce@gmail.com>
2025-05-18 02:48:45 +0100

    logo design

--- resources/views/layouts/layouts_dash/header.blade.php
+++ resources/views/layouts/layouts_dash/header.blade.php
@@ -65,7 +65,7 @@
                 <li class="hide-phone app-search">
                     <form role="search" action="o.p" method="get">
                         <input type="search" name="search" class="form-control top-search mb-0"
-                            placeholder="Search here..." value="{{ request('search') }}">
+                            placeholder="Rechercher ici..." value="{{ request('search') }}">
                         <button type="submit"><i class="iconoir-search"></i></button>
                     </form>
                 </li>
